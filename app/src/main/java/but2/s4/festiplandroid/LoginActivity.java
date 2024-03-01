f02e0aca05891571bde950fa46810bc2f9d03a9d
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-03-01 16:02:07 +0100

    Création projet + début frontend login

---
+++ app/src/main/java/but2/s4/festiplandroid/LoginActivity.java
@@ -0,0 +1,13 @@
+package but2.s4.festiplandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
