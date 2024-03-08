5ede225521de579de7b83110b52b7bae08a7892b
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-03-08 08:11:08 +0100

    Mise à jour UI par rapport à l'état du clavier
    Clavier déployé / caché : l'UI s'adapte pour s'afficher entièrement, et ainsi constamment afficher le bouton de connexion.

--- app/src/main/java/but2/s4/festiplandroid/LoginActivity.java
+++ app/src/main/java/but2/s4/festiplandroid/LoginActivity.java
@@ -3,11 +3,26 @@ package but2.s4.festiplandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        onKeyboardToggling();
+    }
+
+    /**
+     * Resize layout when keyboard is toggled
+     * to keep showing the login button.
+     */
+    private void onKeyboardToggling() {
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams
+                        .SOFT_INPUT_STATE_HIDDEN
+                | WindowManager.LayoutParams
+                        .SOFT_INPUT_ADJUST_RESIZE);
     }
 }
