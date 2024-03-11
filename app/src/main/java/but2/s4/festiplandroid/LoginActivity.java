1d880e036c3cc6f5acb6b17cc5f289a78653bb1e
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-03-11 10:32:44 +0100

    Ajout méthode tentative connexion
    + gestion des erreurs en cours avec bouchons

--- app/src/main/java/but2/s4/festiplandroid/LoginActivity.java
+++ app/src/main/java/but2/s4/festiplandroid/LoginActivity.java
@@ -4,14 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.Console;
+
+import but2.s4.festiplandroid.errors.Error;
+import but2.s4.festiplandroid.navigation.Navigator;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextView error;
+
+    private Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        error = findViewById(R.id.login_form__error);
+
+        loginButton = findViewById(R.id.login_form__login_button);
+        loginButton.setOnClickListener(v -> attemptLogin());
+
         onKeyboardToggling();
+    }
+
+    private void attemptLogin() {
+        boolean stub = false;
+
+        if (stub) {
+            if (this.error.getVisibility() == TextView.GONE) {
+                this.error.setVisibility(TextView.VISIBLE);
+            }
+
+            Navigator.toActivity(this, this.getClass());
+        } else {
+            String introduction,
+                   message;
+
+            introduction = this.getText(R.string.error_introduction).toString();
+            message = this.getText(R.string.login_form_error_invalid_credentials).toString();
+
+            this.error.setVisibility(TextView.VISIBLE);
+            this.error.setText(Error.getPreparedMessage(introduction, message));
+        }
     }
 
     /**
