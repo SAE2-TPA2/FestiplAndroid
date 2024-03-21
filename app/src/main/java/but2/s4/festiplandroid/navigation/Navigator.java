14f063edb4fdc98b5ab0549c5abd06ccb29e57bb
François SP <francois-sp@gmx.fr>
2024-03-21 12:04:56 +0100

    AJout redirection avec extras

--- app/src/main/java/but2/s4/festiplandroid/navigation/Navigator.java
+++ app/src/main/java/but2/s4/festiplandroid/navigation/Navigator.java
@@ -4,8 +4,22 @@ import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashMap;
+
 public class Navigator {
     public static void toActivity(AppCompatActivity activity, Class<?> target) {
         activity.startActivity(new Intent(activity, target));
     }
+
+    public static void toActivity(AppCompatActivity activity, Class<?> target, HashMap<String, String> extras) {
+        Intent intent = new Intent(activity, target);
+
+        for (String key : extras.keySet()) {
+            intent.putExtra(key, extras.get(key));
+        }
+
+        activity.startActivity(intent);
+    }
+
+
 }
