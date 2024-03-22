0b281f702d83d5ebd291dc0925397dc2e8d93411
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-03-22 10:18:50 +0100

    Create Show.java

---
+++ app/src/main/java/but2/s4/festiplandroid/festivals/Show.java
@@ -0,0 +1,43 @@
+package but2.s4.festiplandroid.festivals;
+
+public class Show {
+    private int idSpectacle;
+
+    private String titreSpectacle;
+
+    private String descriptionSpectacle;
+
+    private int idImage;
+
+    public int getIdSpectacle() {
+        return idSpectacle;
+    }
+
+    public void setIdSpectacle(int idSpectacle) {
+        this.idSpectacle = idSpectacle;
+    }
+
+    public String getTitreSpectacle() {
+        return titreSpectacle;
+    }
+
+    public void setTitreSpectacle(String titreSpectacle) {
+        this.titreSpectacle = titreSpectacle;
+    }
+
+    public String getDescriptionSpectacle() {
+        return descriptionSpectacle;
+    }
+
+    public void setDescriptionSpectacle(String descriptionSpectacle) {
+        this.descriptionSpectacle = descriptionSpectacle;
+    }
+
+    public int getIdImage() {
+        return idImage;
+    }
+
+    public void setIdImage(int idImage) {
+        this.idImage = idImage;
+    }
+}
