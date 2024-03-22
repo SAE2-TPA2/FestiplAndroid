78d6303c1eadece6dd34e65e3766727d6b28c2e9
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-03-22 10:18:46 +0100

    Implementation API vue détails festival

--- app/src/main/java/but2/s4/festiplandroid/DetailsActivity.java
+++ app/src/main/java/but2/s4/festiplandroid/DetailsActivity.java
@@ -3,10 +3,14 @@ package but2.s4.festiplandroid;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -19,6 +23,7 @@ import but2.s4.festiplandroid.api.FestiplanApi;
 import but2.s4.festiplandroid.festivals.Festival;
 import but2.s4.festiplandroid.festivals.Organizer;
 import but2.s4.festiplandroid.festivals.Scene;
+import but2.s4.festiplandroid.festivals.Show;
 import but2.s4.festiplandroid.navigation.Navigator;
 
 /**
@@ -48,6 +53,8 @@ extends AppCompatActivity {
 
     private LinearLayout scenesList;
 
+    private LinearLayout showsList;
+
     private Festival currentFestival;
 
     /**
@@ -68,45 +75,44 @@ extends AppCompatActivity {
         this.startDate = this.findViewById(R.id.start);
         this.endDate = this.findViewById(R.id.end);
         this.organizersList = this.findViewById(R.id.organizers_list);
+        this.showsList = this.findViewById(R.id.shows_list);
         this.scenesList = this.findViewById(R.id.scenes_list);
 
         this.loadFestivalObject();
     }
 
     private void loadFestivalObject() {
-        ApiResponse callback = new ApiResponse() {
-            @Override
-            public void onResponse(String response) {
-                Gson gson = new Gson();
-                Type festivalType;
-                List<Festival> festivalFound;
+        ApiResponse callback = response -> {
+            Gson gson = new Gson();
+            Type festivalType;
+            List<Festival> festivalFound;
 
-                festivalType = new TypeToken<List<Festival>>() {}.getType();
-                festivalFound = gson.fromJson(response, festivalType);
+            festivalType = new TypeToken<List<Festival>>() {}.getType();
+            festivalFound = gson.fromJson(response, festivalType);
 
-                if (festivalFound.isEmpty()) {
-                    Navigator.toActivity(DetailsActivity.this,
-                                         LoginActivity.class);
-                    // TODO: remplacer LoginActivity par la page des festivals programmés
-                    return;
-                }
-
-                System.out.println(festivalFound.get(0));
-
-                currentFestival = festivalFound.get(0);
-
-                festivalName.setText(currentFestival
-                                      .getNomFestival());
-                description.setText(currentFestival
-                                     .getDescriptionFestival());
-                startDate.setText(currentFestival
-                                   .getDateDebutFestival());
-                endDate.setText(currentFestival
-                                 .getDateFinFestival());
-
-                updateOrganizersList();
-                updateScenesList();
+            if (festivalFound.isEmpty()) {
+                Navigator.toActivity(DetailsActivity.this,
+                                     LoginActivity.class);
+                // TODO: remplacer LoginActivity par la page des festivals programmés
+                return;
             }
+
+            System.out.println(festivalFound.get(0));
+
+            currentFestival = festivalFound.get(0);
+
+            festivalName.setText(currentFestival
+                                  .getNomFestival());
+            description.setText(currentFestival
+                                 .getDescriptionFestival());
+            startDate.setText(currentFestival
+                               .getDateDebutFestival());
+            endDate.setText(currentFestival
+                             .getDateFinFestival());
+
+            updateOrganizersList();
+            updateScenesList();
+            updateShowsList();
         };
 
         FestiplanApi.createFestivalApiListener(1, callback);
@@ -116,44 +122,41 @@ extends AppCompatActivity {
         final String PATTERN_LIST
             = "<ul>%s</ul>";
 
-        ApiResponse callback = new ApiResponse() {
-            @Override
-            public void onResponse(String response) {
-                Gson gson = new Gson();
-                Type organizersType;
-                List<Organizer> organizersFound;
-                StringBuilder listContent;
-                String currentOrganizerCompleteName;
+        ApiResponse callback = response -> {
+            Gson gson = new Gson();
+            Type organizersType;
+            List<Organizer> organizersFound;
+            StringBuilder listContent;
+            String currentOrganizerCompleteName;
 
-                organizersType = new TypeToken<List<Organizer>>() {}.getType();
-                organizersFound = gson.fromJson(response, organizersType);
+            organizersType = new TypeToken<List<Organizer>>() {}.getType();
+            organizersFound = gson.fromJson(response, organizersType);
 
-                if (organizersFound.isEmpty()) {
-                    return;
-                }
-
-                listContent = new StringBuilder();
-
-                for (Organizer currentOrganizer: organizersFound) {
-                    currentOrganizerCompleteName
-                        = currentOrganizer.getPrenomUser()
-                          + " "
-                          + currentOrganizer.getNomUser();
-
-                    listContent.append("<li>&nbsp;&nbsp;&nbsp;")
-                               .append(currentOrganizerCompleteName)
-                               .append("</li>");
-                }
-
-                listContent
-                    = new StringBuilder(
-                        String.format(PATTERN_LIST,
-                                      listContent.toString()));
-
-                organizersList
-                    .setText(Html.fromHtml(listContent.toString(),
-                                           Html.FROM_HTML_MODE_COMPACT));
+            if (organizersFound.isEmpty()) {
+                return;
             }
+
+            listContent = new StringBuilder();
+
+            for (Organizer currentOrganizer: organizersFound) {
+                currentOrganizerCompleteName
+                    = currentOrganizer.getPrenomUser()
+                      + " "
+                      + currentOrganizer.getNomUser();
+
+                listContent.append("<li>&nbsp;&nbsp;&nbsp;")
+                           .append(currentOrganizerCompleteName)
+                           .append("</li>");
+            }
+
+            listContent
+                = new StringBuilder(
+                    String.format(PATTERN_LIST,
+                                  listContent.toString()));
+
+            organizersList
+                .setText(Html.fromHtml(listContent.toString(),
+                                       Html.FROM_HTML_MODE_COMPACT));
         };
 
         FestiplanApi.createFestivalOrganizersApiListener(this.currentFestival,
@@ -161,39 +164,39 @@ extends AppCompatActivity {
     }
 
     private void updateScenesList() {
-        ApiResponse callback = new ApiResponse() {
-            @Override
-            public void onResponse(String response) {
-                final String SCENE_DESCRIPTION_PATTERN
-                    = "%s places · %s scène";
+        ApiResponse callback = response -> {
+            final String SCENE_DESCRIPTION_PATTERN
+                = "%s places · %s scène";
 
-                Gson gson = new Gson();
-                Type scenesType;
-                List<Scene> scenesFound;
-                Scene scene;
+            Gson gson = new Gson();
+            Type scenesType;
+            List<Scene> scenesFound;
 
-                LinearLayout sceneLayout;
-                TextView sceneName;
-                TextView sceneDescription;
+            LinearLayout sceneLayout;
+            TextView sceneName;
+            TextView sceneDescription;
 
-                String sceneDescriptionContent,
-                       sceneSize;
+            String sceneDescriptionContent,
+                   sceneSize;
 
-                scenesType = new TypeToken<List<Scene>>() {}.getType();
-                scenesFound = gson.fromJson(response, scenesType);
+            scenesType = new TypeToken<List<Scene>>() {}.getType();
+            scenesFound = gson.fromJson(response, scenesType);
 
-                if (scenesFound.isEmpty()) {
-                    return;
-                }
+            if (scenesFound.isEmpty()) {
+                return;
+            }
 
-                scene = scenesFound.get(0);
-
+            for (Scene scene: scenesFound) {
                 sceneLayout = new LinearLayout(DetailsActivity.this);
+                sceneLayout.getContext()
+                           .setTheme(R.style.details_scenes_list_item);
                 sceneLayout.setOrientation(LinearLayout.VERTICAL);
+
+                System.out.println(sceneLayout.getOrientation());
 
                 sceneName = new TextView(DetailsActivity.this);
                 sceneName.setTextAppearance(
-                    R.style.details_organizers_list_item_title);
+                    R.style.details_scenes_list_item_title);
                 sceneName.setText(scene.getNomScene());
 
                 switch (scene.getTailleScene()) {
@@ -221,17 +224,123 @@ extends AppCompatActivity {
 
                 sceneDescription = new TextView(DetailsActivity.this);
                 sceneDescription.setTextAppearance(
-                    R.style.details_organizers_list_item_description);
+                    R.style.details_scenes_list_item_description);
                 sceneDescription.setText(sceneDescriptionContent);
 
                 sceneLayout.addView(sceneName);
                 sceneLayout.addView(sceneDescription);
 
-                scenesList.addView(sceneLayout);
+                this.scenesList.addView(sceneLayout);
             }
         };
 
         FestiplanApi.createFestivalScenesApiListener(this.currentFestival,
                                                      callback);
+    }
+
+    private void updateShowsList() {
+        ApiResponse callback = response -> {
+            Gson gson = new Gson();
+            Type showsType;
+            List<Show> showsFound;
+
+            showsType = new TypeToken<List<Show>>() {}.getType();
+            showsFound = gson.fromJson(response, showsType);
+
+            if (showsFound.isEmpty()) {
+                return;
+            }
+
+            for (Show currentShow: showsFound) {
+                this.createShowRow(currentShow);
+            }
+        };
+    }
+
+    private void createShowRow(Show currentShow) {
+        ConstraintLayout showLayout,
+                         showDates;
+
+        TextView showTitle,
+                 showCategory,
+                 showDescription,
+                 showStartDate,
+                 showEndDate;
+
+        ImageView showDatesAngle;
+
+        ConstraintLayout.LayoutParams showLayoutParams;
+
+        showLayoutParams
+                = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT,
+                                                    ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        showLayout = new ConstraintLayout(this);
+        showLayout.setLayoutParams(showLayoutParams);
+
+        this.newShowPicture(showLayout);
+
+        /*
+        showDatesAngle = new ImageView(DetailsActivity.this);
+        showDatesAngle.getContext().setTheme(R.style.details_shows_list_item_dates_angle);
+        showDatesAngle.setImageResource(R.drawable.angle_down_dates);
+
+        ConstraintLayout.LayoutParams showDatesAngleParams
+                = new ConstraintLayout.LayoutParams(
+                100,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT);
+
+        showDatesAngleParams.startToStart = ConstraintLayout.LayoutParams.PARENT_ID;
+        showDatesAngleParams.bottomToBottom = R.id.show_start;
+        showDatesAngleParams
+        */
+
+        this.showsList.addView(showLayout);
+    }
+
+    /**
+     * Ajout d'une image pour un spectacle.
+     *
+     * @param showLayout
+     */
+    private void newShowPicture(ConstraintLayout showLayout) {
+        View showPicture;
+
+        ConstraintSet showPictureSet;
+
+        showPicture = new View(this);
+        showPicture.getContext().setTheme(R.style.details_shows_list_item_picture);
+
+        showPictureSet = new ConstraintSet();
+        showPictureSet.clone(showLayout);
+        showPictureSet.connect(
+                showPicture.getId(),
+                ConstraintSet.TOP,
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.TOP,
+                0);
+        showPictureSet.connect(
+                showPicture.getId(),
+                ConstraintSet.START,
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.START,
+                0);
+        showPictureSet.applyTo(showLayout);
+    }
+
+    private void newShowInformation(ConstraintLayout showLayout,
+                                    Show currentShow) {
+
+        ConstraintLayout showInformation;
+
+        ConstraintLayout.LayoutParams showInformationParams;
+
+        showInformation = new ConstraintLayout(this);
+        showInformation.getContext().setTheme(R.style.details_shows_list_item);
+
+        showInformationParams = new ConstraintLayout.LayoutParams(
+                0,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        showInformationParams.topToTop = ConstraintLayout.LayoutParams.MATCH_PARENT;
+        // TODO: showInformationParams.startToEnd =
     }
 }
