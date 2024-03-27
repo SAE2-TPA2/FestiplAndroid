87410293da500b09d1568ca15c7872e651dbfe1a
Alexandreiut <alexandre.brouzes@iut-rodez.fr>
2024-03-27 15:12:13 +0100

    rename des méthodes de l'utilisateur

--- app/src/main/java/but2/s4/festiplandroid/adaptater/FestivalAdapter.java
+++ app/src/main/java/but2/s4/festiplandroid/adaptater/FestivalAdapter.java
@@ -59,7 +59,7 @@ public class FestivalAdapter extends RecyclerView.Adapter<FestivalAdapter.Festiv
         });
         holder.buttonAddToFavorites.setOnClickListener(v -> {
             ImageButton imageButton = (ImageButton) v;
-            int idUser = User.getInstance().getId();
+            int idUser = User.getInstance().getIdUser();
             if(festival.getFavorite()) {
                 imageButton.setImageResource(R.drawable.favorites_deselected);
                 final String[] apiResponse = new String[1];
