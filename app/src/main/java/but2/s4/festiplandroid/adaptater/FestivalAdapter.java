7f29b61ed6b27c0c5ab1cd324563dd73fc3f6904
Alexandreiut <alexandre.brouzes@iut-rodez.fr>
2024-03-29 03:02:13 +0100

    adaptateur good mis appart les images et l'appel api pour les  favories

--- app/src/main/java/but2/s4/festiplandroid/adaptater/FestivalAdapter.java
+++ app/src/main/java/but2/s4/festiplandroid/adaptater/FestivalAdapter.java
@@ -1,22 +1,31 @@
 package but2.s4.festiplandroid.adaptater;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
+import java.util.List;
 
+import but2.s4.festiplandroid.DetailsActivity;
 import but2.s4.festiplandroid.R;
+import but2.s4.festiplandroid.api.ApiResponse;
+import but2.s4.festiplandroid.api.FestiplanApi;
 import but2.s4.festiplandroid.festivals.Festival;
+import but2.s4.festiplandroid.session.User;
 
 public class FestivalAdapter extends RecyclerView.Adapter<FestivalAdapter.FestivalViewHolder> {
-    private ArrayList<Festival> festivalList;
 
-    public FestivalAdapter(ArrayList<Festival> festivalList) {
+    private List<Festival> festivalList;
+
+    public FestivalAdapter(List<Festival> festivalList) {
         this.festivalList = festivalList;
     }
 
@@ -30,7 +39,52 @@ public class FestivalAdapter extends RecyclerView.Adapter<FestivalAdapter.Festiv
     @Override
     public void onBindViewHolder(@NonNull FestivalViewHolder holder, int position) {
         Festival festival = festivalList.get(position);
-        holder.bind(festival);
+        holder.nomFestival.setText(festival.getNomFestival());
+        holder.categorieFestival.setText(festival.getCategorieFestival());
+        holder.dateDebutFestival.setText(festival.getDateDebutFestival());
+        holder.dateFinFestival.setText(festival.getDateFinFestival());
+        holder.buttonShowDetails.setId(festival.getIdFestival());
+        holder.buttonShowDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), DetailsActivity.class);
+                    intent.putExtra("ID_EXTRA", festival.getDateFinFestival());
+                    v.getContext().startActivity(intent);
+                }
+            });
+        holder.buttonAddToFavorites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageButton imageButton = (ImageButton) v;
+                int idUser = User.getInstance().getIdUser();
+//                if (festival.getFavorite()) {
+//                    imageButton.setImageResource(R.drawable.favorites_deselected);
+//                    final String[] apiResponse = new String[1];
+//                    ApiResponse response = new ApiResponse() {
+//                        @Override
+//                        public void onResponse(String response) {
+//                            apiResponse[0] = response;
+//                            System.out.println(apiResponse[0]);
+//                        }
+//                        };
+//                    FestiplanApi.deleteFavoritesFestivalsDeleteListener(idUser, festival.getIdFestival(), response);
+//
+//                } else {
+//                    imageButton.setImageResource(R.drawable.favorites_selected);
+//                    final String[] apiResponse = new String[1];
+//                    ApiResponse response = new ApiResponse() {
+//                        @Override
+//                        public void onResponse(String response) {
+//                            apiResponse[0] = response;
+//                            System.out.println(apiResponse[0]);
+//                        }
+//                    };
+//                    FestiplanApi.createFavoritesFestivalsPostListener(idUser, festival.getIdFestival(), response);
+//                }
+            }
+
+        });
+
     }
 
     @Override
@@ -39,18 +93,25 @@ public class FestivalAdapter extends RecyclerView.Adapter<FestivalAdapter.Festiv
     }
 
     public static class FestivalViewHolder extends RecyclerView.ViewHolder {
-        private TextView nomTextView;
-        private TextView categorieTextView;
+        ImageView imageFestival;
+        TextView nomFestival;
+        TextView categorieFestival;
+        TextView dateDebutFestival;
+        TextView dateFinFestival;
+        Button buttonShowDetails;
+        ImageButton buttonAddToFavorites;
+
+
 
         public FestivalViewHolder(@NonNull View itemView) {
             super(itemView);
-            nomTextView = itemView.findViewById(R.id.nom_festival);
-            categorieTextView = itemView.findViewById(R.id.categorie_festival);
-        }
-
-        public void bind(Festival festival) {
-            nomTextView.setText(festival.getNomFestival());
-            categorieTextView.setText(festival.getCategorieFestival());
+            imageFestival = itemView.findViewById(R.id.image_festival);
+            nomFestival = itemView.findViewById(R.id.nom_festival);
+            categorieFestival = itemView.findViewById(R.id.categorie_festival);
+            dateDebutFestival = itemView.findViewById(R.id.date_debut_festival);
+            dateFinFestival = itemView.findViewById(R.id.date_fin_festival);
+            buttonShowDetails = itemView.findViewById(R.id.button_show_details);
+            buttonAddToFavorites = itemView.findViewById(R.id.button_add_to_favorites);
         }
     }
 }
